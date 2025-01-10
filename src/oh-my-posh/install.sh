8c8b498c70b405de86244574c64bde95d5d13043
Justin Ludlow <justinmludlow@gmail.com>
2025-01-10 00:18:51 +0000

    Add oh-my-posh/ devcontainer-feature.json and install.sh

---
+++ src/oh-my-posh/install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+echo "Activate feature 'oh-my-posh'"
+
+curl -s https://ohmyposh.dev/install.sh | bash -s -- -d /usr/local/bin
+
+oh-my-posh font install JetBrainsMono
+
+if [ -n $CONFIG ]; then
+	echo 'eval "$(oh-my-posh init bash --config $CONFIG)"' >> /etc/profile
+else
+	echo 'eval "$(oh-my-posh init bash)"' >> /etc/profile
+fi
+. /etc/profile
