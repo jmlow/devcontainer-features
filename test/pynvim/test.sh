f42770381f4026f5056b8ed8bd2bf2580589a016
Justin Ludlow <justinmludlow@gmail.com>
2025-11-24 01:33:35 -0700

    Update versions and dependencies for lazygit, lazyvim, mise, and specify features; Adjust pynvim test command

--- test/pynvim/test.sh
+++ test/pynvim/test.sh
@@ -3,5 +3,5 @@ set -e
 
 source dev-container-features-test-lib
 
-check "pynvim install" bash -c "pip show pynvim |& grep -vqz 'not found'"
+check "pynvim install" bash -c "uv pip show pynvim |& grep -vqz 'not found'"
 reportResults
