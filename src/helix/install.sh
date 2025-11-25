1b820780d5f548b0a38cc314dee049b9ab7f8c69
Justin Ludlow <justinmludlow@gmail.com>
2025-11-24 18:30:54 -0700

    Swap to Biome and Neovim; Deprecate helix; Update to actions/checkout@v5; Clean up test.yaml

--- src/helix/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "Activate feature 'helix'"
-
-add-apt-repository -y ppa:maveonair/helix-editor
-apt update
-apt install -y helix
--- test/helix/test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-source dev-container-features-test-lib
-
-check "helix install" bash -c "hx -V |& grep -vqz 'command not found'"
-reportResults
