a7dd678039eac7c4f6dbfdd1b4036fae87c63228
Justin Ludlow <justinmludlow@gmail.com>
2025-11-30 23:34:53 -0700

    Clean up nvm/install.sh

--- src/nvm/install.sh
+++ src/nvm/install.sh
@@ -5,8 +5,8 @@ echo "Activate feature 'nodejs'"
 
 mkdir -p "$NVM_DIR"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion"
 
 nvm install --lts
 nvm alias default 'lts/*'
